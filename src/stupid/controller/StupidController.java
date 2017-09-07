ccb39639fe760ace39d74a7dff6684d32fdbf70f
Outrider99 <outrider.w@gmail.com>
2017-09-07 13:53:49 -0600

    Added firstScanner, my first scanner into StupidController

--- src/stupid/controller/StupidController.java
+++ src/stupid/controller/StupidController.java
@@ -1,9 +1,20 @@
 package stupid.controller;
 
+import java.util.Scanner;
+
 public class StupidController
 {
 	public void start()
 	{
 		System.out.println("Hello? Yes, this is Wyatt.");
+	}
+	
+	private void testScanner()
+	{
+		Scanner firstScanner;
+		firstScanner = new Scanner(System.in);
+		System.out.println("Hello! you scanned me. Say yes");
+		String answer = firstScanner.nextLine();
+		System.out.println("Oh yes, you typed " + answer);
 	}
 }
